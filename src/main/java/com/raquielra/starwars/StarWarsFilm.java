89a0dcde5b847ca4c27daa27060ec6335e727e1c
Raquiel Ribeiro <ribeiro.raquiel@gmail.com>
2025-11-20 18:51:12 -0300

    feat: Atualiza POJO StarWarsFilm com campos director, producer e opening_crawl

--- src/main/java/com/raquielra/starwars/StarWarsFilm.java
+++ src/main/java/com/raquielra/starwars/StarWarsFilm.java
@@ -4,26 +4,82 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public class StarWarsFilm {
-    
+
     @JsonProperty("title")
     private String title;
 
     @JsonProperty("episode_id")
     private int episodeId;
-    
+
     @JsonProperty("release_date")
     private String releaseDate;
 
     @JsonProperty("characters")
     private List<String> characters;
-    
-    public String getTitle() { return title; }
-    public int getEpisodeId() { return episodeId; }
-    public String getReleaseDate() { return releaseDate; }
-    public List<String> getCharacters() { return characters; }
-    
-    public void setTitle(String title) { this.title = title; }
-    public void setEpisodeId(int episodeId) { this.episodeId = episodeId; }
-    public void setReleaseDate(String releaseDate) { this.releaseDate = releaseDate; }
-    public void setCharacters(List<String> characters) { this.characters = characters; }
+
+    @JsonProperty("director")
+    private String director;
+
+    @JsonProperty("producer")
+    private String producer;
+
+    @JsonProperty("opening_crawl")
+    private String openingCrawl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getEpisodeId() {
+        return episodeId;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public List<String> getCharacters() {
+        return characters;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public String getOpeningCrawl() {
+        return openingCrawl;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setEpisodeId(int episodeId) {
+        this.episodeId = episodeId;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public void setCharacters(List<String> characters) {
+        this.characters = characters;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public void setOpeningCrawl(String openingCrawl) {
+        this.openingCrawl = openingCrawl;
+    }
+
 }
