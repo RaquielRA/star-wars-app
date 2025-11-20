67b74b5798ca94d166618c1c0ef45960be952292
Raquiel Ribeiro <ribeiro.raquiel@gmail.com>
2025-11-20 14:36:44 -0300

    refactor: Remove CommandLineRunner para converter projeto em API REST

--- src/main/java/com/raquielra/starwars/StarWarsApplication.java
+++ src/main/java/com/raquielra/starwars/StarWarsApplication.java
@@ -11,23 +11,4 @@ public class StarWarsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StarWarsApplication.class, args);
 	}
-    
-    @Bean
-    public CommandLineRunner run(StarWarsService movieService) {
-        return args -> {
-            int filmId = 4; 
-            StarWarsFilm film = movieService.fetchFilmById(filmId);
-
-            if (film != null) {
-                System.out.println("\n--- FILME STAR WARS ENCONTRADO ---");
-                System.out.println("Título: " + film.getTitle());
-                System.out.println("Episódio: " + film.getEpisodeId());
-                System.out.println("Lançamento: " + film.getReleaseDate());
-                System.out.println("Total de Personagens no Filme: " + film.getCharacters().size());
-                System.out.println("------------------------------------");
-            } else {
-                System.out.println("\n--- Falha na busca ou API fora do ar ---");
-            }
-        };
-    }
 }
